10324ec7d56ee7b0e6305f4377128c0f12d91d98
Ali Faizan <Ali Faizan>
2016-02-01 06:04:00 -0500

    Testing of the Consumer Type

---
+++ ConsumerTest.java
@@ -0,0 +1,29 @@
+package main.java;
+
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class ConsumerTest {
+
+	public Consumer testConsumer;
+	public Document testDocument;
+
+    @Before
+    public void setUp() throws Exception {
+    	//testConsumer = new Consumer(new Simulation(), "testName","testTaste");
+    	//testDocument = new Document("testDocument", "testTag", new Producer(simulation, "testProducer", "testTag"));
+    }
+
+    @Test
+    public void testAct() throws Exception {
+    	//assert testConsumer.act() == 0;
+    }
+
+    @Test
+    public void testCalculatePayoff() throws Exception {
+    	//assert testConsumer.calculatePayoff() == 0;
+    }
+}
