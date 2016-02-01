f465c937f4bd53ce648a386ec06048f15495a808
alifaizan <alifaizann95@gmail.com>
2016-02-01 06:30:09 -0500

    Test class for ProducerTest

---
+++ ProducerTest.java
@@ -0,0 +1,61 @@
+package main.java;
+//Name: Zameer Masjedee
+//Student Number: 100888893
+//Date: November 5th,2015
+//Version #: 1
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ProducerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testAct() throws Exception {
+
+    }
+
+    @Test
+    public void testNewDoc() throws Exception {
+
+    }
+
+    @Test
+    public void testSearch() throws Exception {
+
+    }
+
+    @Test
+    public void testUpdateLikesAndFollowers() throws Exception {
+
+    }
+
+    @Test
+    public void testCalculatePayoff() throws Exception {
+
+    }
+
+    @Test
+    public void testGetFollowers() throws Exception {
+
+    }
+
+    @Test
+    public void testSetFollowers() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCreated() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCreated() throws Exception {
+
+    }
+}
